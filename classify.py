43b859419dc101fcc6db997369200c19d2d4212d
nvpnathan <nathan79@gmail.com>
2024-03-08 09:36:03 -0700

    refactor classify

--- classify.py
+++ classify.py
@@ -6,58 +6,59 @@ class Classify:
         self.project_id = project_id
         self.bearer_token = bearer_token
 
+    def _parse_classification_results(self,
+                                      response: dict,
+                                      document_id: str,
+                                      validate_classification: bool):
+        try:
+            classification_results = response.json()
+            if validate_classification:
+                return classification_results
+
+            document_type_id = None
+            classification_confidence = None
+            for result in classification_results['classificationResults']:
+                if result['DocumentId'] == document_id:
+                    document_type_id = result['DocumentTypeId']
+                    classification_confidence = result['Confidence']
+                    break
+            if document_type_id:
+                print(f"Document Type ID: {document_type_id}, Confidence: {classification_confidence}\n")
+                return document_type_id
+
+            print("Document ID not found in classification results.")
+            return None
+        except ValueError as ve:
+            print(f"Error parsing JSON response: {ve}")
+            return None
+
 
     def classify_document(self,
                           document_id: str,
                           classifier: str,
                           classification_prompts: dict,
                           validate_classification: bool = False):
-        # Define the API endpoint for document classification
         api_url = f"{self.base_url}{self.project_id}/classifiers/{classifier}/classification?api-version=1"
-
-        # Define the headers with the Bearer token and content type
         headers = {
             "Authorization": f"Bearer {self.bearer_token}",
             "accept": "text/plain",
             "Content-Type": "application/json"
         }
-
         data = {
             "documentId": f"{document_id}",
             **(classification_prompts or {})
         }
 
         try:
-            # Make the POST request
             response = requests.post(api_url, json=data, headers=headers, timeout=60)
 
             if response.status_code == 200:
                 print("Document successfully classified!")
-                # Try parsing the JSON response
-                try:
-                    classification_results = response.json()
-                    if validate_classification:
-                        return classification_results
-                    else:
-                        document_type_id = None
-                        classification_confidence = None
-                        for result in classification_results['classificationResults']:
-                            if result['DocumentId'] == document_id:
-                                document_type_id = result['DocumentTypeId']
-                                classification_confidence = result['Confidence']
-                                break
-                        if document_type_id:
-                            print(f"Document Type ID: {document_type_id}, Confidence: {classification_confidence}\n")
-                            return document_type_id
-                        
-                        print("Document ID not found in classification results.")
-                        return None
-                except ValueError as ve:
-                    print(f"Error parsing JSON response: {ve}")
-                    return None
-            else:
-                print(f"Error: {response.status_code} - {response.text}")
-                return None
+                return self._parse_classification_results(response, document_id, 
+                                                          validate_classification)
+
+            print(f"Error: {response.status_code} - {response.text}")
+            return None
 
         except Exception as e:
             print(f"An error occurred during classification: {e}")
