36a47d558091bf4a3498ba06b6ae8f124eff1533
nvpnathan <nathan79@gmail.com>
2024-09-28 09:48:52 -0700

    refactored validation for error handling

--- src/validate.py
+++ src/validate.py
@@ -25,8 +25,7 @@ class Validate:
             "Content-Type": "application/json",
         }
 
-        # Define the payload data
-        payload = {
+        data = {
             "documentId": document_id,
             "actionTitle": f"Validate - {extractor_id}",
             "actionPriority": "Medium",
@@ -40,7 +39,8 @@ class Validate:
 
         try:
             # Make the POST request to initiate validation
-            response = requests.post(api_url, json=payload, headers=headers, timeout=60)
+            response = requests.post(api_url, json=data, headers=headers, timeout=60)
+            response.raise_for_status()  # Raise an exception for HTTP errors
 
             if response.status_code == 202:
                 print("\nExtraction Validation request sent!")
@@ -50,15 +50,21 @@ class Validate:
                 operation_id = response_data.get("operationId")
 
                 # Wait until the validation operation is completed
-                validation_result = self.submit_extraction_validation_request(
-                    extractor_id, operation_id
-                )
+                if operation_id:
+                    validation_result = self.submit_extraction_validation_request(
+                        extractor_id, operation_id
+                    )
+                print("Extraction Validation Complete!\n")
                 return validation_result
-            else:
-                print(f"Error: {response.status_code} - {response.text}")
+            print(f"Error: {response.status_code} - {response.text}")
+            return None
 
-        except Exception as e:
-            print(f"An error occurred during validation: {e}")
+        except requests.exceptions.RequestException as e:
+            print(f"Error submitting extraction validation request: {e}")
+            # Handle network-related errors
+        except Exception as ex:
+            print(f"An error occurred during extraction validation: {ex}")
+            # Handle any other unexpected errors
 
     def submit_extraction_validation_request(
         self, extractor_id: str, operation_id: str
@@ -72,41 +78,56 @@ class Validate:
             "Authorization": f"Bearer {self.bearer_token}",
         }
 
-        while True:
-            response = requests.get(api_url, headers=headers, timeout=60)
-            response_data = response.json()
-            if response_data["status"] == "Succeeded":
-                print("Extraction Validation request submitted successfully!")
-                while True:
-                    response = requests.get(api_url, headers=headers, timeout=60)
-                    response_data = response.json()
-                    # Check the status inside actionData
-                    action_data_status = response_data["result"]["actionData"]["status"]
-                    print(
-                        f"Validate Document Extraction action status: {action_data_status}"
-                    )
-                    if action_data_status == "Unassigned":
+        try:
+            while True:
+                response = requests.get(api_url, headers=headers, timeout=60)
+                response_data = response.json()
+                if response_data["status"] == "Succeeded":
+                    print("Extraction Validation request submitted successfully!")
+                    while True:
+                        response = requests.get(api_url, headers=headers, timeout=60)
+                        response_data = response.json()
+                        # Check the status inside actionData
+                        action_data_status = response_data["result"]["actionData"][
+                            "status"
+                        ]
                         print(
-                            "Validation Document Extraction is unassigned. Waiting..."
+                            f"Validate Document Extraction action status: {action_data_status}"
                         )
-                    elif action_data_status == "Pending":
-                        print("Validate Document Extraction in progress. Waiting...")
-                    elif action_data_status == "Completed":
-                        print("Validate Document Extraction is completed.")
-                        return response_data
-                    else:
-                        print("Unknown validation action status.")
-                    time.sleep(5)  # Wait for 5 seconds before checking again
+                        if action_data_status == "Unassigned":
+                            print(
+                                "Validation Document Extraction is unassigned. Waiting..."
+                            )
+                        elif action_data_status == "Pending":
+                            print(
+                                "Validate Document Extraction in progress. Waiting..."
+                            )
+                        elif action_data_status == "Completed":
+                            print("Validate Document Extraction is completed.")
+                            return response_data
+                        else:
+                            print("Unknown validation action status.")
+                        time.sleep(5)  # Wait for 5 seconds before checking again
 
-            elif response_data["status"] == "NotStarted":
-                print("Extraction Validation request has not started. Waiting...")
-            elif response_data["status"] == "Running":
-                print("Extraction Validation request is in progress. Waiting...")
-            elif response_data["status"] == "Unassigned":
-                print("Extraction Validation request is unassigned. Waiting...")
-            else:
-                print("Extraction Validation request failed...")
-                return None
+                elif response_data["status"] == "NotStarted":
+                    print("Extraction Validation request has not started. Waiting...")
+                elif response_data["status"] == "Running":
+                    print("Extraction Validation request is in progress. Waiting...")
+                elif response_data["status"] == "Unassigned":
+                    print("Extraction Validation request is unassigned. Waiting...")
+                else:
+                    print("Extraction Validation request failed...")
+                    return None
+        except requests.exceptions.RequestException as e:
+            print(f"Error submitting extraction validation request: {e}")
+            # Handle network-related errors
+        except KeyError as ke:
+            print(f"KeyError: {ke}")
+            # Handle missing keys in the response JSON
+        except Exception as ex:
+            print(f"An error occurred during extraction validation: {ex}")
+            # Handle any other unexpected errors
+            return None
 
     def validate_classification_results(
         self,
@@ -129,7 +150,7 @@ class Validate:
         }
 
         # Define the payload data
-        payload = {
+        data = {
             "documentId": document_id,
             "actionTitle": f"Validate - {document_type_id}",
             "actionPriority": "Medium",
@@ -143,7 +164,8 @@ class Validate:
 
         try:
             # Make the POST request to initiate validation
-            response = requests.post(api_url, json=payload, headers=headers, timeout=60)
+            response = requests.post(api_url, json=data, headers=headers, timeout=60)
+            response.raise_for_status()  # Raise an exception for HTTP errors
 
             if response.status_code == 202:
                 print("\nClassification Validation request sent!")
@@ -153,9 +175,12 @@ class Validate:
                 operation_id = response_data.get("operationId")
 
                 # Wait until the validation operation is completed
-                validation_result = self.submit_classification_validation_request(
-                    operation_id
-                )
+                if operation_id:
+                    validation_result = self.submit_classification_validation_request(
+                        operation_id
+                    )
+                print("Classification Validation Complete!\n")
+
                 if validation_result:
                     document_type_id = validation_result["result"][
                         "validatedClassificationResults"
@@ -163,11 +188,15 @@ class Validate:
                     return document_type_id
                 print("Validation result is None.")
                 return None
-            else:
-                print(f"Error: {response.status_code} - {response.text}")
+            print(f"Error: {response.status_code} - {response.text}")
+            return None
 
-        except Exception as e:
-            print(f"An error occurred during validation: {e}")
+        except requests.exceptions.RequestException as e:
+            print(f"Error submitting classification validation request: {e}")
+            # Handle network-related errors
+        except Exception as ex:
+            print(f"An error occurred during classification validation: {ex}")
+            # Handle any other unexpected errors
 
     def submit_classification_validation_request(
         self, operation_id: str
@@ -178,41 +207,57 @@ class Validate:
             "accept": "application/json",
             "Authorization": f"Bearer {self.bearer_token}",
         }
+        try:
+            while True:
+                response = requests.get(api_url, headers=headers, timeout=60)
+                response_data = response.json()
+                if response_data["status"] == "Succeeded":
+                    print("Classification Validation request submitted successfully!")
+                    while True:
+                        response = requests.get(api_url, headers=headers, timeout=60)
+                        response_data = response.json()
+                        # Check the status inside actionData
+                        action_data_status = response_data["result"]["actionData"][
+                            "status"
+                        ]
+                        print(
+                            f"Validate Document Classification action status: {action_data_status}"
+                        )
+                        if action_data_status == "Unassigned":
+                            print(
+                                "Validation Document Classification is unassigned. Waiting..."
+                            )
+                        elif action_data_status == "Pending":
+                            print(
+                                "Validate Document Classification in progress. Waiting..."
+                            )
+                        elif action_data_status == "Completed":
+                            print("Validate Document Classification is completed.")
+                            return response_data
+                        else:
+                            print("Unknown validation action status.")
+                        time.sleep(5)  # Wait for 5 seconds before checking again
 
-        while True:
-            response = requests.get(api_url, headers=headers, timeout=60)
-            response_data = response.json()
-            if response_data["status"] == "Succeeded":
-                print("Classification Validation request submitted successfully!")
-                while True:
-                    response = requests.get(api_url, headers=headers, timeout=60)
-                    response_data = response.json()
-                    # Check the status inside actionData
-                    action_data_status = response_data["result"]["actionData"]["status"]
+                elif response_data["status"] == "NotStarted":
                     print(
-                        f"Validate Document Classification action status: {action_data_status}"
+                        "Classification Validation request has not started. Waiting..."
                     )
-                    if action_data_status == "Unassigned":
-                        print(
-                            "Validation Document Classification is unassigned. Waiting..."
-                        )
-                    elif action_data_status == "Pending":
-                        print(
-                            "Validate Document Classification in progress. Waiting..."
-                        )
-                    elif action_data_status == "Completed":
-                        print("Validate Document Classification is completed.")
-                        return response_data
-                    else:
-                        print("Unknown validation action status.")
-                    time.sleep(5)  # Wait for 5 seconds before checking again
-
-            elif response_data["status"] == "NotStarted":
-                print("Classification Validation request has not started. Waiting...")
-            elif response_data["status"] == "Running":
-                print("Classification Validation request is in progress. Waiting...")
-            elif response_data["status"] == "Unassigned":
-                print("Classification Validation request is unassigned. Waiting...")
-            else:
-                print("Classification Validation request failed...")
-                return None
+                elif response_data["status"] == "Running":
+                    print(
+                        "Classification Validation request is in progress. Waiting..."
+                    )
+                elif response_data["status"] == "Unassigned":
+                    print("Classification Validation request is unassigned. Waiting...")
+                else:
+                    print("Classification Validation request failed...")
+                    return None
+        except requests.exceptions.RequestException as e:
+            print(f"Error submitting extraction validation request: {e}")
+            # Handle network-related errors
+        except KeyError as ke:
+            print(f"KeyError: {ke}")
+            # Handle missing keys in the response JSON
+        except Exception as ex:
+            print(f"An error occurred during extraction validation: {ex}")
+            # Handle any other unexpected errors
+            return None
